f28554638ca267b59b6962c4c306c9e9446bcf02
k3csk3 <szilagyi.viktor89@gmail.com>
2022-05-23 17:23:31 +0200

    sok új dolog és példa az easy-ben

--- js az aloldalaknak/javaScript.js
+++ js az aloldalaknak/javaScript.js
@@ -1,9 +1,49 @@
-let amountInput = document.querySelector("input[name='amount-input']")
+function calcAmount() {
+    let price = 1200;
+    let amountInput = document.querySelector("input[name='amount-input']");
+    let amount = parseInt(amountInput.value) * price;
+    alert("Fizetendő: " + amount+"ft");
+}
 
-/*az amountInputnak megadtam, hogy az értéke az az input legyen, aminek a neve az, 
-hogy "amount-input".Tehát, ha nyomok egy amountInputot, leírja a tulajdonságait,
-ha pedig az "amountInput.value"-t kérem, akkor azt, amit beleírtak.*/
+function calcAmountTwo() {
+    let price = 1200;
+    let amountInput = document.querySelector("input[name='amount-inputTwo']");
+    let showAmount = document.querySelector("span.show-amount");
+    let amount = parseInt(amountInput.value) * price;
+    
+    showAmount.innerHTML = amount
+}
 
-let ár = 1200;
+function calcAmountThree() {
+    let price = 1200;
+    let amountInput = document.querySelector("input[name='amount-inputThree']");
+    let showAmount = document.querySelector("span.show-amountThree");
+    let amountNumber = parseInt(amountInput.value)
 
-let végösszeg = parseInt(amountInput.value) * ár;
+    if (amountNumber > 10) {
+        alert("Maximum 10 terméket vásárolhatunk!")
+    } else if (amountNumber < 1) {
+        alert("Minimum 1 terméket kell vásárolnunk!")
+    } else {
+        let amount = parseInt(amountInput.value) * price;
+        showAmount.innerHTML = amount
+    }
+}
+
+function calcAmountFour() {
+    let price = 1200;
+    let amountInput = document.querySelector("input[name='amount-inputFour']");
+    let showAmount = document.querySelector("span.show-amountFour");
+    let amountNumber = parseInt(amountInput.value)
+    
+    amountNumber = isNaN(amountNumber) ? 0 : amountNumber
+
+    if (amountNumber > 10) {
+        alert("Maximum 10 terméket vásárolhatunk!")
+    } else if (amountNumber < 1) {
+        alert("Minimum 1 terméket kell vásárolnunk!")
+    } else {
+        let amount = parseInt(amountInput.value) * price;
+        showAmount.innerHTML = amount
+    }
+}
