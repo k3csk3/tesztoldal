018b10fc515fe9680270fb6ae7fcb792334e34b9
k3csk3 <szilagyi.viktor89@gmail.com>
2022-05-23 12:45:01 +0200

    miért nem jó?

--- js az aloldalaknak/javaScript.js
+++ js az aloldalaknak/javaScript.js
@@ -1,4 +1,9 @@
-let userAge = String(33)
-//Itt pl. jobb lenne mégis, ha szám lenne.
-userAge = Number(userAge)
-//Nem bíztam a javaScriptre, hogy definiáljon, én mondom megmi az érték típusa.
+let amountInput = document.querySelector("input[name='amount-input']")
+
+/*az amountInputnak megadtam, hogy az értéke az az input legyen, aminek a neve az, 
+hogy "amount-input".Tehát, ha nyomok egy amountInputot, leírja a tulajdonságait,
+ha pedig az "amountInput.value"-t kérem, akkor azt, amit beleírtak.*/
+
+let ár = 1200;
+
+let végösszeg = parseInt(amountInput.value) * ár;
