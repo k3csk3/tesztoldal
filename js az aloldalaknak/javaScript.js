acfd4fc8f88119d27db8815d206f9020f6c73daf
k3csk3 <szilagyi.viktor89@gmail.com>
2022-05-25 21:24:43 +0200

    már nem is tudom

--- js az aloldalaknak/javaScript.js
+++ js az aloldalaknak/javaScript.js
@@ -47,3 +47,34 @@ function calcAmountFour() {
         showAmount.innerHTML = amount
     }
 }
+
+
+
+/* Itt ideraktam egy egyszerűsítést, ráadásul ha a showSumPriceban kimarad egy érték, akkor 0-át írna, de azt meg nem írhat, 
+tehát jön az alert gomb. Ebben ez segít: amountNumber = amountNumber || 0;
+
+function calcAmountFour() {
+    let price = 1200;
+    let amountInput = document.querySelector("input[name='amount-inputFour']");
+    let amountNumber = parseInt(amountInput.value);
+    amountNumber = isNaN(amountNumber) ? 0 : amountNumber;
+    
+    showNumber = isNaN(amountNumber) ? 0 : amountNumber;
+
+    showSumPrice(price, amountNumber)
+}
+
+function showSumPrice (x, y) {
+    amountNumber = amountNumber || 0;
+    let showAmount = document.querySelector("span.show-amountFour");
+    if (x > 10) {
+        alert("Maximum 10 terméket vásárolhatunk!")
+    } else if (x < 1) {
+        alert("Minimum 1 terméket kell vásárolnunk!")
+    } else {
+        let amount = x * y;
+        showAmount.innerHTML = amount
+    }
+}
+
+*/
