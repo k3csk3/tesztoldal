2bbb2d916b06fa878a005b7783e36233f26be8c0
k3csk3 <szilagyi.viktor89@gmail.com>
2022-06-02 19:28:15 +0200

    nightmarefull

--- ideiglenes.js
+++ ideiglenes.js
@@ -1,1 +1,5 @@
-var div = document.createElement('div');
+<div>
+  <span onclick="this.parentElement.style.display = 'none';">x</span>
+</div>
+
+//Kattintásra a div-nek fogja a style.display tulajdonságát "none" -ra állítani. Tehát a kattintott elem szülőjének.
